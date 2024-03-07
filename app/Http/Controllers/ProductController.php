ee455ab476319345b4212356967b58851a32bce3
Mixchennie <nhung.phan25@student.passerellesnumeriques.org>
2024-03-07 07:50:12 +0700

    Laravel1

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products=Product::all();
+        return view('homepage',compact('products'));
+    }
+
+    public function showProducts()
+    {
+        $products=Product::all();
+        return view('product_type',compact('products'));
+    }
+
+    public function showPricing(){
+        $products=Product::all();
+        return view('pricing',compact('products'));
+    }
+
+    public function checkout()
+    {
+        return view('checkout');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product=Product::find($id);
+        // dd($product);
+        return view('product_detail', compact('product'));
+
+        // return view ('car-detail', compact('car'));
+    }
+
+    // {
+    //     $car=Car::find($id);
+    //     // dd($car);
+    //     return view ('car-detail', compact('car'));
+    // }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
